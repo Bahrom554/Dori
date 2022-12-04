c427e15a3a5157a34e189f32ada60f892311d615
Bahrom554 <bahrombozorboev554@gmail.com>
2022-12-05 01:54:19 +0500

    ds

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,7 @@ Route::get('/',function (){
     return redirect(route('login'));
 });
 
-Route::get('/storage-link',function (){
- $targetFolder=storage_path('app/public');
- $linkfolder=$_SERVER['DOCUMENT_ROOT'].'/storage';
- symlink($targetFolder,$linkfolder);
-});
+
 Route::group(['middleware' => 'auth'], function () {
     Route::group(['namespace' => 'Filemanager', 'prefix' => 'admin/filemanager'], function () {
         Route::get('/', 'FilemanagerController@index');
